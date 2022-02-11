d9054a0c27ca5a1ced1f81dc2e1bae7d8003d0fe
yogita <yogitasraut25@gmail.com>
2022-02-11 18:34:57 +0530

    Refactored: Two-dimensional array

--- src/com/bridgelab/day5functionalprograms/TwoDArray.java
+++ src/com/bridgelab/day5functionalprograms/TwoDArray.java
@@ -7,33 +7,32 @@ public class TwoDArray {
 	public static void arrayOperations() {
 		Scanner sc = new Scanner(System.in);
 		PrintWriter writer = new PrintWriter(System.out);
-		System.out.println("Enter the Rows and Columns to form a MATRIX:");
+		System.out.println("! Enter the Rows and Columns to form a MATRIX: ");
 		int rows = sc.nextInt();
 		int colm = sc.nextInt();
-		int[][] array = new int[rows][colm]; //declaring array
+		int[][] array = new int[rows][colm]; // declaring array
 		System.out.println("Enter array elements: ");
-		//taking inputs for rows
-		for(int i=0; i<rows; i++) {
-			//taking inputs for columns
-			for(int j=0; j<colm; j++) {
+		// taking inputs for rows
+		for (int i = 0; i < rows; i++) {
+			// taking inputs for columns
+			for (int j = 0; j < colm; j++) {
 				array[i][j] = sc.nextInt();
 			}
 		}
 		sc.close();
-		//printing array elements
-		for(int i=0; i<rows; i++) {
-			for(int j=0; j<colm; j++) {
-				writer.print(array[i][j]+" ");
+		// printing array elements
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < colm; j++) {
+				writer.print(array[i][j] + " ");
 
 			}
 			writer.println();
 		}
 		writer.close();
-	}	
+	}
 
 	public static void main(String[] args) {
 		arrayOperations();
 
 	}
 }
-
