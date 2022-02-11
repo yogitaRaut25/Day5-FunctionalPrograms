bdbc219b044ddd957a8e1ade409504bad27a4a5a
yogita <yogitasraut25@gmail.com>
2022-02-11 18:36:23 +0530

    Refactored: Wind Chill logic

--- src/com/bridgelab/day5functionalprograms/WindChill.java
+++ src/com/bridgelab/day5functionalprograms/WindChill.java
@@ -1,21 +1,21 @@
 package com.bridgelab.day5functionalprograms;
 
 public class WindChill {
-	public static double temp,windSpeed,windChill;
-	public static void checkWindChill(double temp,double windSpeed) {
-		if(temp<50 || windSpeed<120 || windSpeed>3)
-		{
-		 windChill=35.74 + (0.6215*temp) + (0.4275*temp * (Math.pow (windSpeed,0.16))-35.75 * (Math.pow(windSpeed,0.16)));
-			System.out.println("WindChill= "+windChill);
-		}
-		else 
-			System.out.println(" Invalid input  ");
+	public static double temp, windSpeed, windChill;
+
+	public static void checkWindChill(double temp, double windSpeed) {
+		if (temp < 50 || windSpeed < 120 || windSpeed > 3) {
+			windChill = 35.74 + (0.6215 * temp)
+					+ (0.4275 * temp * (Math.pow(windSpeed, 0.16)) - 35.75 * (Math.pow(windSpeed, 0.16)));
+			System.out.println("WindChill= " + windChill);
+		} else
+			System.out.println(" Invalid input!!!  ");
 	}
 
 	public static void main(String[] args) {
-		//taking command-line arguments
-		double temp=Double.parseDouble(args[0]);
-		double windSpeed=Double.parseDouble(args[1]);
-		checkWindChill(temp,windSpeed);//calling function
+		// taking command-line arguments
+		double temp = Double.parseDouble(args[0]);
+		double windSpeed = Double.parseDouble(args[1]);
+		checkWindChill(temp, windSpeed);// calling function
 	}
 }
